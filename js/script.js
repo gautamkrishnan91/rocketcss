359171cbaf2060ea99607c61a5a2db270e3d6b21
gkthegr8 <gkthegr8@gmail.com>
2013-09-08 13:41:58 +0530

    Load website completely at once

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 function detectBrowser(){
+	document.getElementById('anim').style.display = "block";
 	var isMac = navigator.platform.toUpperCase().indexOf('MAC')>=0;
 	if(isMac){
   		document.getElementById('anim').style.webkitAnimationName = "STAR-MOVE1";
